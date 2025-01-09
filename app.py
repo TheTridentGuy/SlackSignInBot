80c33d608623681317f41b17ef3c860abfb551b6
Aiden Bohlander <aiden.bohlander@gmail.com>
2025-01-09 13:55:08 -0800

    add some logging stuff

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import json
 import pathlib
 import gsheets
 from pathlib import Path
-from config import bot_token, secret, form_url, sheet_id, sheet_range
+from config import bot_token, secret, form_url, sheet_id, sheet_range, channel
 
 
 client = slack_sdk.WebClient(token=bot_token)
@@ -18,12 +18,21 @@ if pathlib.Path(data_file).exists():
     user_data = json.load(open(data_file, "r"))
 
 
+client.chat_postMessage(channel=channel, text=":cardinalpog: Sign-in bot is online!")
+
+
+def report(message):
+    client.chat_postMessage(channel=channel, text=message)
+    return message
+
+
 @app.route("/commands/register", methods=["POST"])
 def register():
     user = flask.request.values.get("user_id")
     email = flask.request.values.get("text")
     user_data[user] = email
     json.dump(user_data, open(data_file, "w"))
+    report(f"<@{user}> registered email {email}")
     return f":white_check_mark: Registered your email as {email}. You may now sign in/out with /signin, or check your status with /signin-status"
 
 
@@ -32,13 +41,20 @@ def signin():
     user = flask.request.values.get("user_id")
     email = user_data.get(user)
     if not email:
+        report(f"<@{user}> tried to sign in without registering")
         return f":x: Please register your email with /signin-register [email]"
-    data = get_signin_status(email)
-    response = requests.post(f"{form_url}/formResponse?emailAddress={email}")
-    if response.status_code == 200:
-        return f":white_check_mark: Sucessfully submitted form with email {email}, you are now signed {'out' if data else 'in'}."
-    else:
-        return f":x: Unknown error submitting form with email '{email}', http status: {response.status_code}"
+    try:
+        data = get_signin_status(email)
+        response = requests.post(f"{form_url}/formResponse?emailAddress={email}")
+        if response.status_code == 200:
+            return f":white_check_mark: Sucessfully submitted form with email {email}, you are now signed {'out' if data else 'in'}."
+        else:
+
+            return report(f":x: Unknown error submitting form with email '{email}', http status: {response.status_code}")
+    except Exception as e:
+        report(f":x: Unknown error signing in/out with email '{email}', {e}")
+        return f"Something went wrong singing in/out with email {email}, please use the form for now."
+
 
 
 @app.route("/commands/status", methods=["POST"])
@@ -46,12 +62,17 @@ def status():
     user = flask.request.values.get("user_id")
     email = user_data.get(user)
     if not email:
-        return f":x: Please register your email with /sregister [email]"
-    data = get_signin_status(email)
-    if get_signin_status(email):
-        return f":information_source: {data[0]} {data[1]} ({email}) has been in since {data[3]}"
-    else:
-        return f":information_source: {email} is currently signed out"
+        report(f"<@{user}> tried to check status without registering")
+        return f"Please register your email with /sregister [email]"
+    try:
+        data = get_signin_status(email)
+        if get_signin_status(email):
+            return f":information_source: {data[0]} {data[1]} ({email}) has been in since {data[3]}"
+        else:
+            return f":information_source: {email} is currently signed out"
+    except Exception as e:
+        report(f":x: Unknown error checking status with email '{email}', {e}")
+        return f"Something went wrong checking status with email {email}, please use the form for now."
 
 
 def get_signin_status(email):
