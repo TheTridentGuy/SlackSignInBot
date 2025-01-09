2f48ecc928b9305ad82cec857ec89a437a3ab737
Aiden Bohlander <aiden.bohlander@gmail.com>
2025-01-09 14:24:57 -0800

    add hints for unnecessary data

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import pathlib
 import gsheets
 from pathlib import Path
 from config import bot_token, secret, form_url, sheet_id, sheet_range, channel
+import re
 
 
 client = slack_sdk.WebClient(token=bot_token)
@@ -33,6 +34,9 @@ def register():
     user_data[user] = email
     json.dump(user_data, open(data_file, "w"))
     report(f"<@{user}> registered email {email}")
+    if not re.match(r"^[^@+]+(\+[^@]+)?@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$", email):
+        report(f"<@{user}> attempted to register invalid email '{email}'")
+        return f":x: '{email}' does not appear to be a valid email, please try again."
     return f":white_check_mark: Registered your email as {email}. You may now sign in/out with /signin, or check your status with /signin-status"
 
 
@@ -47,7 +51,7 @@ def signin():
         data = get_signin_status(email)
         response = requests.post(f"{form_url}/formResponse?emailAddress={email}")
         if response.status_code == 200:
-            return f":white_check_mark: Sucessfully submitted form with email {email}, you are now signed {'out' if data else 'in'}."
+            return f":white_check_mark: Sucessfully submitted form with email {email}, you are now signed {'out' if data else 'in'}." + "\nHINT: you don't need to provide any info (i.e. an email) to this command." if flask.request.values.get("text") else ""
         else:
 
             return report(f":x: Unknown error submitting form with email '{email}', http status: {response.status_code}")
@@ -67,9 +71,9 @@ def status():
     try:
         data = get_signin_status(email)
         if get_signin_status(email):
-            return f":information_source: {data[0]} {data[1]} ({email}) has been in since {data[3]}"
+            return f":information_source: {data[0]} {data[1]} ({email}) has been in since {data[3]}" + "\nHINT: you don't need to provide any info (i.e. an email) to this command." if flask.request.values.get("text") else ""
         else:
-            return f":information_source: {email} is currently signed out"
+            return f":information_source: {email} is currently signed out" + "\nHINT: you don't need to provide any info (i.e. an email) to this command." if flask.request.values.get("text") else ""
     except Exception as e:
         report(f":x: Unknown error checking status with email '{email}', {e}")
         return f"Something went wrong checking status with email {email}, please use the form for now."
